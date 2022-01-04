4d3e80894423ebd603ad20e14866b4a5d0c39dfb
Kelly Alzate <kejoh07@gmail.com>
2022-01-04 16:43:17 +0100

    review eslem

--- components/header.js
+++ components/header.js
@@ -1,20 +1,10 @@
 import styles from './header.module.css'
-import Image from 'next/image'
-import Head from 'next/head'
 
-export default function Header() {
-    return (
-        <div className={styles.principal}>
-            <Head>
-                <Image
-                    priority
-                    src="/images/koi.jpeg"
-                    className={utilStyles.borderCircle}
-                    height={50}
-                    width={50}
-                    alt={name}
-                />
-            </Head>
-        </div>
-    )
-} 
+const Header = () => (
+    <div className="header-content">
+        <div className="header-title-text">Do you want contact me?</div>
+        <input type="button" value="Contact" className="header-button-add"/>
+    </div>
+);
+
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import Header from '../components/header'
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Head>
         <title>{siteTitle}</title>
       </Head>
+      <Header />
       <section className={utilStyles.headingMd}>
         <p>
           Hello, I'm Kelly. I'm a full stack developer. I work with React, Next, Angular{' '}
